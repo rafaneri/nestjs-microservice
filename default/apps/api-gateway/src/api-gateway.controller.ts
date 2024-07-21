16ca0eea05de1c815777e201e7fbbf0e400445d9
Rafael Neri <rafael.neri@gmail.com>
2024-07-21 02:49:53 -0300

    Refactoring ApiGatewayModule and Controller
    Improve the code consistence and responsibility

--- default/apps/api-gateway/src/api-gateway.controller.ts
+++ default/apps/api-gateway/src/api-gateway.controller.ts
@@ -1,17 +1,12 @@
-import { Controller, Get, Inject, Param, ParseIntPipe } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
-import { ClientAlias } from './client.enum';
-import { ActionType } from '@wallet/domain';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { GetBalanceService } from './services/get-balance/get-balance.service';
 
 @Controller('wallet')
 export class ApiGatewayController {
-  constructor(
-    @Inject(ClientAlias.BALANCE)
-    private getBalanceClient: ClientProxy,
-  ) {}
+  constructor(private getBalanceService: GetBalanceService) {}
 
   @Get('/:wallet/balance')
   getBalance(@Param('wallet', ParseIntPipe) wallet: number) {
-    return this.getBalanceClient.send(ActionType.GET_BALLANCE, wallet);
+    return this.getBalanceService.send(wallet);
   }
 }
--- default/apps/api-gateway/src/api-gateway.module.ts
+++ default/apps/api-gateway/src/api-gateway.module.ts
@@ -1,27 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ApiGatewayController } from './api-gateway.controller';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { ClientAlias } from './client.enum';
+import { ServicesModule } from './services/services.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    ClientsModule.registerAsync([
-      {
-        name: ClientAlias.BALANCE,
-        imports: [ConfigModule],
-        useFactory: (configService: ConfigService) => ({
-          transport: Transport.TCP,
-          options: {
-            host: configService.get('TCP_HOST'),
-            port: configService.get('GET_BALANCE_PORT'),
-          },
-        }),
-        inject: [ConfigService],
-      },
-    ]),
-  ],
+  imports: [ServicesModule],
   controllers: [ApiGatewayController],
 })
 export class ApiGatewayModule {}
